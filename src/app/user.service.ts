305eb6c733964c1ebc0e44d457d9c55192443a94
taran910 <tarandeep910@gmail.com>
2020-04-07 04:33:14 -0900

    anotherone

--- src/app/user.service.ts
+++ src/app/user.service.ts
@@ -1,45 +1,60 @@
 
 export class ListService{
-    list={"array":[
+    list=[
         {
             "name":"Billy Lee",
             "position":"Web Developer",
             "office":"Detroit",
-            "salary":"$50000"
+            "salary":"$50000",
+            "edit":"Edit",
+            "delete":"Delete"
 
         },
         {
             "name":"John Doe",
             "position":"Manager",
             "office":"Troy",
-            "salary":"$90000"
+            "salary":"$90000",
+            "edit":"Edit",
+            "delete":"Delete"
         },
         {
             "name":"James Baxter",
             "position":"IT Support",
             "office":"Detroit",
-            "salary":"$30000"
+            "salary":"$30000",
+            "edit":"Edit",
+            "delete":"Delete"
         },
         {
             "name":"Jimmy Lee",
             "position":"Web Developer",
             "office":"Detroit",
-            "salary":"$50000"
+            "salary":"$50000",
+            "edit":"Edit",
+            "delete":"Delete"
         },
         {
             "name":"Nick Wess",
             "position":"Sales",
             "office":"Ann Arbor",
-            "salary":"$40000"
+            "salary":"$40000",
+            "edit":"Edit",
+            "delete":"Delete"
         },
         {
             "name":"Sarah Deets",
             "position":"Graphic Designer",
             "office":"Ann Arbor",
-            "salary":"$40000"
+            "salary":"$40000",
+            "edit":"Edit",
+            "delete":"Delete"
         }
-    ]}
+    ]
     getList(){
         return this.list;
+    }
+    chang(li){
+        this.list=li;
     }
 }
--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ListService } from '../user.service';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-user',
@@ -8,11 +9,54 @@ import { ListService } from '../user.service';
   providers: [ListService]
 })
 export class UserComponent{
-  emp;
+  li;
+  dup;
+  index:any;
+  nam:any;
+  pos:any;
+  sal:any;
+  off:any;
+  ele:any;
   constructor(private service:ListService) {
-    this.emp=service.getList();
-    console.log(this.emp);
+    this.li=service.getList();
+    console.log(this.li);
+    this.dup=this.li;
+    console.log(this.dup)
    }
+  openForm(i){
+    console.log(i);
+    this.index=i;
+    document.getElementById('popup').style.display="block";
+  }
+  closeForm(){
+    this.li[this.index].name=this.nam;
+    this.li[this.index].position=this.pos;
+    this.li[this.index].salary=this.sal;
+    this.li[this.index].office=this.off;
+    console.log(this.index,this.nam);
+    document.getElementById('popup').style.display="none";
+  }
+  deleteEmp(i,n){
+  this.li.splice(i,1);  
+  }
+  Form(){
+    document.getElementById('popup1').style.display="block";
+   
+  }
+  save(){
+    console.log(this.nam);
+    this.ele={
+      "name":this.nam,
+      "position":this.pos,
+      "office":this.off,
+      "salary":this.sal,
+      "edit":"Edit",
+      "delete":"Delete"
+    }
+    console.log(this.ele);
+    this.li.push(this.ele);
+    document.getElementById('popup1').style.display="none";
+  }
+  }
   
-  
-}
+
